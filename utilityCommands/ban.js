5c639e9355b7f1354065865fca5d6b51acdb0410
mkdeptrai <166988497+mkdeptrai@users.noreply.github.com>
2024-05-08 16:49:55 +0700

    Update ban.js

--- utilityCommands/ban.js
+++ utilityCommands/ban.js
@@ -9,18 +9,18 @@ module.exports = {
     description: 'Ban a user from the server',
     execute(message, args) {
         if (!message.member.permissions.has('BAN_MEMBERS')) {
-            return message.reply('❌ You do not have permission to use this command.');
+            return message.reply('Bạn không có quyền để sử dụng lệnh này !!!');
         }
         const user = message.mentions.users.first();
         if (!user) {
-            return message.reply('❌ You need to mention a user to ban.');
+            return message.reply('Bạn cần chỉ định người dùng để cấm.');
         }
         const member = message.guild.members.cache.get(user.id);
         member.ban();
         const embed = new EmbedBuilder()
             .setColor('#FF0000')
-            .setTitle('User Banned ✅')
-            .setDescription(`▶️ ${user.tag} has been banned from the server by ${message.author.tag}.`)
+            .setTitle('Người dùng bị ban ✅')
+            .setDescription(`▶️ ${user.tag} đã bị cút khỏi server bởi ${message.author.tag}.`)
             .setTimestamp();
 
         message.reply({ embeds: [embed] });
@@ -28,23 +28,3 @@ module.exports = {
 };
 
 
-/*
-
-   MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   FOR EMOJIS EITHER YOU CAN EDIT OR JOIN OUR DISCORD SERVER 
-   SO WE ADD BOT TO OUR SERVER SO YOU GET ANIMATED EMOJIS.
-
-   DISCORD SERVER : https://discord.gg/FUEHs7RCqz
-   YOUTUBE : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-
-   FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/c4kaW2sSbm ]
-*/
